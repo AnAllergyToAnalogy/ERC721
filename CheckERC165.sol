454529da33a369679c70d28249ac1dd68f319c74
AnAllergyToAnalogy <andrewdangerparker@gmail.com>
2018-04-13 15:55:22 +0100

    Update CheckERC165.sol

--- CheckERC165.sol
+++ CheckERC165.sol
@@ -3,11 +3,11 @@ pragma solidity ^0.4.21;
 import "./standard/ERC165.sol";
 
 contract CheckERC165 is ERC165 {
+    function CheckERC165() public {
+        supportedInterfaces[this.supportedInterfaces.selector] = true;
+    }
     mapping (bytes4 => bool) internal supportedInterfaces;
     function supportsInterface(bytes4 interfaceID) external view returns (bool){
         return supportedInterfaces[interfaceID];
-    }
-    function addInterface(bytes4 interfaceID) internal{
-        supportedInterfaces[interfaceID] = true;
     }
 }
