4fb63a9be64c9ee89de19db073434e836526c7a9
AnAllergyToAnalogy <andrewdangerparker@gmail.com>
2018-04-14 02:13:48 +0100

    Added some comments

--- CheckERC165.sol
+++ CheckERC165.sol
@@ -3,10 +3,19 @@ pragma solidity ^0.4.21;
 import "./standard/ERC165.sol";
 
 contract CheckERC165 is ERC165 {
+
     function CheckERC165() public {
         supportedInterfaces[this.supportedInterfaces.selector] = true;
     }
+
     mapping (bytes4 => bool) internal supportedInterfaces;
+    
+    /// @notice Query if a contract implements an interface
+    /// @param interfaceID The interface identifier, as specified in ERC-165
+    /// @dev Interface identification is specified in ERC-165. This function
+    ///  uses less than 30,000 gas.
+    /// @return `true` if the contract implements `interfaceID` and
+    ///  `interfaceID` is not 0xffffffff, `false` otherwise
     function supportsInterface(bytes4 interfaceID) external view returns (bool){
         return supportedInterfaces[interfaceID];
     }
