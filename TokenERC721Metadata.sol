c058a4882d7a43ba7bd4f4d2e742ec91f3b2c36a
AnAllergyToAnalogy <andrewdangerparker@gmail.com>
2018-04-14 03:14:52 +0100

    Added comments, added dependancy
    Forgot to make it actually extend the ERC721Metadata interface. Also make some comments nicer.

--- TokenERC721Metadata.sol
+++ TokenERC721Metadata.sol
@@ -1,14 +1,20 @@
 pragma solidity ^0.4.21;
 
 import "./TokenERC721.sol";
+import "./standard/ERC721Metadata.sol";
 
-contract TokenERC721Metadata is TokenERC721 {
+contract TokenERC721Metadata is TokenERC721, ERC721Metadata {
 
+    /// @notice Contract constructor
+    /// @param _initialSupply The number of tokens to mint initially (see TokenERC721)
+    /// @param name The name of the NFT
+    /// @param symbol The symbol for the NFT
+    /// @param uriBase The base for the tokens' URI. Assumes metadata will be in the form "something/tokenId"
     function TokenERC721Metadata(uint _initialSupply, string name, string symbol, string uriBase) public TokenERC721(_initialSupply){
         _name = name;
         _symbol = symbol;
         _uriBase = uriBase;
-        
+
         //Add to ERC165 Interface Check
         supportedInterfaces[
             this.name.selector ^
@@ -21,10 +27,16 @@ contract TokenERC721Metadata is TokenERC721 {
     string private _symbol;
     string private _uriBase;
 
+    /// @notice A distinct Uniform Resource Identifier (URI) for a given asset.
+    /// @dev Throws if `_tokenId` is not a valid NFT. URIs are defined in RFC
+    ///  3986. The URI may point to a JSON file that conforms to the "ERC721
+    ///  Metadata JSON Schema".
+    /// @param _tokenId The tokenId of the token of which to retrieve the URI.
+    /// @return (string) The URI of the token.
     function tokenURI(uint256 _tokenId) external view returns (string){
-        uint maxlength = 100;
+        uint maxLength = 100;
         uint tokenId_temp = _tokenId;
-        bytes memory reversed = new bytes(maxlength);
+        bytes memory reversed = new bytes(maxLength);
         uint i = 0;
         while (tokenId_temp != 0) {
             uint remainder = tokenId_temp % 10;
