7e424f8f909b52574415b4c1c783654b604f9e13
Andrew Parker <andrewdangerparker@gmail.com>
2018-05-09 14:21:17 +0100

    changed URI func, mutability of name and symbol

--- TokenERC721Metadata.sol
+++ TokenERC721Metadata.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.4.21;
+pragma solidity ^0.4.22;
 
 import "./TokenERC721.sol";
 import "./standard/ERC721Metadata.sol";
@@ -13,10 +13,10 @@ contract TokenERC721Metadata is TokenERC721, ERC721Metadata {
     /// @param name The name of the NFT
     /// @param symbol The symbol for the NFT
     /// @param uriBase The base for the tokens' URI. Assumes metadata will be in the form "something/tokenId"
-    function TokenERC721Metadata(uint _initialSupply, string name, string symbol, string uriBase) public TokenERC721(_initialSupply){
+    constructor(uint _initialSupply, string name, string symbol, string uriBase) public TokenERC721(_initialSupply){
         _name = name;
         _symbol = symbol;
-        _uriBase = uriBase;
+        _uriBase = bytes(uriBase);
 
         //Add to ERC165 Interface Check
         supportedInterfaces[
@@ -28,7 +28,7 @@ contract TokenERC721Metadata is TokenERC721, ERC721Metadata {
 
     string private _name;
     string private _symbol;
-    string private _uriBase;
+    bytes private _uriBase;
 
     /// @notice A distinct Uniform Resource Identifier (URI) for a given asset.
     /// @dev Throws if `_tokenId` is not a valid NFT. URIs are defined in RFC
@@ -36,35 +36,33 @@ contract TokenERC721Metadata is TokenERC721, ERC721Metadata {
     ///  Metadata JSON Schema".
     /// @param _tokenId The tokenId of the token of which to retrieve the URI.
     /// @return (string) The URI of the token.
-    function tokenURI(uint256 _tokenId) external view returns (string){
+    function tokenURI(uint256 _tokenId) public view returns (string){
+    //Note: changed visibility to public
         uint maxLength = 100;
-        uint tokenId_temp = _tokenId;
         bytes memory reversed = new bytes(maxLength);
         uint i = 0;
-        while (tokenId_temp != 0) {
-            uint remainder = tokenId_temp % 10;
-            tokenId_temp = tokenId_temp / 10;
+        while (_tokenId != 0) {
+            uint remainder = _tokenId % 10;
+            _tokenId /= 10;
             reversed[i++] = byte(48 + remainder);
         }
-        bytes memory inStrb = bytes(_uriBase);
-        bytes memory s = new bytes(inStrb.length + i);
+        bytes memory s = new bytes(_uriBase.length + i);
         uint j;
-        for (j = 0; j < inStrb.length; j++) {
-            s[j] = inStrb[j];
+        for (j = 0; j < _uriBase.length; j++) {
+            s[j] = _uriBase[j];
         }
         for (j = 0; j < i; j++) {
-            s[j + inStrb.length] = reversed[i - 1 - j];
+            s[j + _uriBase.length] = reversed[i - 1 - j];
         }
         return string(s);
     }
 
     /// @notice A descriptive name for a collection of NFTs in this contract
-    function name() external pure returns (string _name){
-        return;
+    function name() external view returns (string _name){
+        return _name;
     }
 
     /// @notice An abbreviated name for NFTs in this contract
-    function symbol() external pure returns (string _symbol){
-        return;
+    function symbol() external view returns (string _symbol){
+        return _symbol;
     }
-}
